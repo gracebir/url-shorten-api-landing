cc26b3b25847ca1642cbbe6307c2e428d6a37779
gracebir <grace.devolop@gmail.com>
2023-02-28 23:47:32 +0200

    overflow layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './components/Banner/Banner';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className='font-poppins text-[1.125rem]'>
+    <div className='font-poppins overflow-x-hidden md:overflow-x-hidden text-[1.125rem] bg-[#fafafa]'>
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
