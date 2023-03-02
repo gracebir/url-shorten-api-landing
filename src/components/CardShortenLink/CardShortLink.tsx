1593f86c68140a28e09d70fc7167ace6f19a5b17
gracebir <grace.devolop@gmail.com>
2023-03-02 11:39:33 +0200

    done the card shorten link

--- src/components/CardShortenLink/CardShortLink.tsx
+++ src/components/CardShortenLink/CardShortLink.tsx
@@ -5,11 +5,11 @@ function CardShortLink() {
     const handleClick = () => setClicked(true)
   return (
     <div className='bg-white mx-8 py-6 md:mx-0 md:px-4 rounded-md flex flex-col md:flex-row md:justify-between md:items-center gap-4'>
-      <p className="px-4">https://www.frontendmentor.io</p>
+      <p className="px-4 text-[1rem] md:text-[1.2rem]">https://www.frontendmentor.io</p>
       <hr  className='block md:hidden'/>
       <div className='px-4 flex flex-col gap-2 md:gap-4 md:flex-row md:items-center'>
         <p className='text-clCyan'>https://rel.link/k4IKyk</p>
-        <button onClick={handleClick} className={`transition ease-in duration-500 ${clicked ? `bg-clViolet`:`bg-clCyan`} py-2 md:px-6 text-white font-bold rounded-md`}>
+        <button onClick={handleClick} className={`transition ease-in duration-500 md:hover:bg-cyan-300 ${clicked ? `bg-clViolet`:`bg-clCyan`} py-2 md:px-6 text-white font-bold rounded-md`}>
             {clicked ? "Copied!":"Copy"}
         </button>
       </div>
