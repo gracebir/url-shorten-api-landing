d78efa61f25b6d33eb4d9dbbb28a03ddeaec91c1
gracebir <grace.devolop@gmail.com>
2023-03-03 21:14:25 +0200

    shorten

--- src/components/Shorten/Shorten.tsx
+++ src/components/Shorten/Shorten.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import CardShortLink from '../CardShortenLink/CardShortLink'
-import ShortenCard from '../ShortenCard/ShortenCard'
+import ShortenCard, { shortType } from '../ShortenCard/ShortenCard'
 
+// const shortens:Array<shortType> = JSON.parse(localStorage.getItem("shorten")!)
+
+let shortDatas:Array<shortType> = []
+console.log("jjsjs",JSON.parse(localStorage.getItem("shorten")!))
 function Shorten() {
-  // eslint-disable-next-line
-    const [shorten, setShorten] = useState("")
+  const [shortData, setShortData] = useState<Array<shortType>>([...shortDatas])
+  console.log(shortData)
   return (
     <div className='min-h-[20vh] bg-gray-300'>
-      <ShortenCard shorten={shorten} setShorten={setShorten}/>
-      <div className='md:container md:mx-auto pt-[7rem] pb-6 md:pt-[6rem] bg-gray-300 flex flex-col gap-4 md:gap-6'>
-        <CardShortLink/>
-        <CardShortLink/>
+      <ShortenCard shortData={shortData} setShortData={setShortData}/>
+      {shortData.length > 0 ? (
+        <div className='md:container md:mx-auto pt-[7rem] pb-6 md:pt-[6rem] bg-gray-300 flex flex-col gap-4 md:gap-6'>
+        {/* <CardShortLink/> */}
+        {shortData?.map((shorten, i)=> (
+          <CardShortLink key={i} full_short_link={shorten.full_short_link} original_link={shorten.original_link}/>
+        ))}
       </div>
+      ): null}
     </div>
   )
 }
