d59f80a80c91593db07fbf3717e072779db43d6e
gracebir <grace.devolop@gmail.com>
2023-03-05 14:23:15 +0200

    removing

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// https://res.cloudinary.com/dnzidlufh/image/upload/v1677652957/bg-shorten-desktop_lm9lz1.svg
-// https://res.cloudinary.com/dnzidlufh/image/upload/v1677652957/bg-shorten-mobile_wzraxw.svg
-
-// https://res.cloudinary.com/dnzidlufh/image/upload/v1677652957/bg-shorten-mobile_wzraxw.svg
-// https://res.cloudinary.com/dnzidlufh/image/upload/v1677652957/bg-shorten-mobile_wzraxw.svg
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
